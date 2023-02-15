2675dc71e95b60f9bca7278278c6f3c57d2d6fda
DivyanshMittal-exe <78041366+DivyanshMittal-exe@users.noreply.github.com>
2023-02-15 14:55:52 +0530

    clean

--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/div/Codes/signal_module/modules.order := {   echo /home/div/Codes/signal_module/signal_module.ko; :; } > /home/div/Codes/signal_module/modules.order
--- .signal_module.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/div/Codes/signal_module/signal_module.ko := ld -r -m elf_x86_64 -z noexecstack --build-id=sha1  -T scripts/module.lds -o /home/div/Codes/signal_module/signal_module.ko /home/div/Codes/signal_module/signal_module.o /home/div/Codes/signal_module/signal_module.mod.o;  true
