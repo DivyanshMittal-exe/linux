369b2566558f828cbf48975f860bf4bdefc7d8a6
DivyanshMittal-exe <78041366+DivyanshMittal-exe@users.noreply.github.com>
2023-04-29 16:34:42 +0530

    Done

--- tester.cpp
+++ tester.cpp
@@ -6,6 +6,24 @@
 #include <fcntl.h>
 #include <cstring>
 
+#include <poll.h>
+
+using namespace std;
+
+#include <sys/ioctl.h>
+
+
+#define LIFO_IOC_MAGIC  'D'
+
+#define LIFO_IOCRESET    _IO(LIFO_IOC_MAGIC, 0)
+
+#define LIFO_DATA_AVBL  _IOR(LIFO_IOC_MAGIC, 1,int)
+#define LIFO_REF_COUNT  _IOR(LIFO_IOC_MAGIC, 2,int)
+
+
+struct pollfd fds[1];
+
+
 
 char message[] = "Hello, this is a test message!";
 
@@ -23,12 +41,47 @@ void writer(int fd) {
 
 void reader(int fd) {
 
+    int count;
+    if (ioctl(fd, LIFO_IOCRESET, &count) == -1) {
+        cout << "Oh no " << endl;
+        // return;
+    }
+
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
     std::cout << "Reader thread started\n";
     int size_to_read = 10*(sizeof(message)-1);
 
     char buffer[size_to_read + 1];
+
+
+    if (ioctl(fd, LIFO_DATA_AVBL, &count) == -1) {
+        cout << "Oh no " << endl;
+        // return;
+    }
+
+    cout << "Data Count :" << count << endl;
+
+    fds[0].fd = fd; 
+    fds[0].events = POLLIN; 
+
+    int ret = poll(fds, 1, 100);
+
+    if (ret == -1) {
+        cout << "Umm" << endl;
+    } else if (ret == 0) {
+        cout << "timeout occurred" << endl;
+    } else {
+        if (fds[0].revents & POLLIN) {
+            cout << "input is available" << endl;
+        }
+
+         if (fds[0].revents & POLLOUT) {
+            cout << "Space to write is available " << endl;
+        }
+
+    }
+
     ssize_t bytes_read = read(fd, buffer, size_to_read);
     if (bytes_read < 0) {
         std::cerr << "Error reading from device\n";
